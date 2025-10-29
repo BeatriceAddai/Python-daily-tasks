6cc97d99b44e9cee579938d91f00f41b946cc943
BeatriceAddai <beatriceaddai59@gmail.com>
2025-10-29 09:54:26 +0000

    Swapping values in variables

---
+++ Day 4.py
@@ -0,0 +1,14 @@
+a = input("a: ")
+b = input("b: ")
+
+#Switching values stored in variables a and b
+c = a   #Declare a temporary variable c to hold he value of a
+a = b 
+b = c 
+
+print(a)
+print(b) 
+
+
+
+
