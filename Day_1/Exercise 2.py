cf6c62d18f9d3ff4458e9012fbcc7508803b9745
BeatriceAddai <beatriceaddai59@gmail.com>
2025-10-31 15:24:15 +0000

    Finished day 1 task

---
+++ Day_1/Exercise 2.py
@@ -0,0 +1,6 @@
+#String Manipulation
+print("Day 2 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
---
+++ Day_1/Exercise 3.py
@@ -0,0 +1,3 @@
+#Calculate the length of the string and print the result
+name = input("Please enter your name? ")
+print(len(name))
