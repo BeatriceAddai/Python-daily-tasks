ac20837575045c565f5317f3da9f2a167741a94c
BeatriceAddai <beatriceaddai59@gmail.com>
2025-11-20 12:34:54 +0000

    Day_6

--- Day_6/Concepts.py
+++ Day_6/Concepts.py
@@ -1,5 +1,12 @@
 # Functions
-def my_function():
+def my_function(): #Defining a function
     print("Hello")
     print("Bye")
-my_function()
+my_function()# calling a function
+
+#While loop is about doing something whilst a condition is true
+#while condition_is_true:
+    #do this
+
+
+   
---
+++ Day_6/Exercise_1.py
@@ -0,0 +1,20 @@
+#Learning functions hurdle exercise.
+
+#def turn_right():
+    #turn_left()
+    #turn_left()
+    #turn_left()
+
+
+#turn_left()
+#move()
+#turn_right()
+#move()
+#turn_right()
+#move()
+#turn_right()
+#move()
+
+
+
+ 
