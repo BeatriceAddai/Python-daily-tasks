94fb96892babdab3760167592d42ce16aeeb2b6e
BeatriceAddai <beatriceaddai59@gmail.com>
2025-11-24 16:55:51 +0000

    Exercise 1 completed

---
+++ Day_7/Exercise_1.py
@@ -0,0 +1,28 @@
+import random
+#Step 1 
+
+word_list = ["ardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+guess = input("Guess a letter: ").lower()
+chosen_word = random.choice(word_list)
+for word in chosen_word:
+    if guess == word:
+        print("right")
+    else:
+        print("wrong")
+    
+
+        
+
+
+# if guess in word_list:
+#      print("right")
+# else:
+#      print("wrong")
+
